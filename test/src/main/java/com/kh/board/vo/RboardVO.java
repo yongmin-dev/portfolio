c32f31a4cc910086e93968ff8466f0b425c4b03a
23das <23das@Brandon>
2020-03-20 01:28:14 +0900

    RboardVO 수정

--- test/src/main/java/com/kh/board/vo/RboardVO.java
+++ test/src/main/java/com/kh/board/vo/RboardVO.java
@@ -2,6 +2,8 @@ package com.kh.board.vo;
 
 import java.sql.Timestamp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
@@ -11,12 +13,18 @@ public class RboardVO {
 	private long bnum; //게시글번호
 	private String rid; // 작성자 ID
 	private String rnickname; //작성자이름
+	
+	@JsonFormat(pattern = "yyyy-MM-dd h:mm a", timezone = "Asia/Seoul")
 	private Timestamp rcdate; //작성일
+	@JsonFormat(pattern = "yyyy-MM-dd h:mm a", timezone = "Asia/Seoul")
 	private Timestamp rudate; // 수정일
 	private String rcontent; //본문내용
 	private int rgroup; //답글그룹
 	private int rstep; // 답변글 단계
 	private int rindent; // 답변글 들여쓰기
 	private int prnum; // 부모댓글번호
+	
+	private int good; // 선호
+	private int bad; // 비선호
 	
 }
