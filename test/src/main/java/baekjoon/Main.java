996ddd5d4c49c8799a5e52e8fccd1c43d9d0a92b
yongmin <dev.min@kakao.com>
2020-03-23 15:58:04 +0900

    백준 문제풀기

--- test/src/main/java/baekjoon/Main.java
+++ test/src/main/java/baekjoon/Main.java
@@ -1,26 +1,18 @@
 package baekjoon;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		int num = 0; // 처음 입력받은 수
-		int sum = 0; // 계산하여 나온 결과
-		int cal = 0; // 반복문을 위하여 계산값 설정
+		String num; // 처음 입력받은 수
 		int cnt = 0; // 최종 반복 횟수
-		Scanner sc = new Scanner(new BufferedInputStream(System.in));
-		num = sc.nextInt();
-		cal = num;
-		while (num != sum) {
-			sum = (cal % 10) * 10 + ((cal / 10) + (cal % 10)) % 10;
-			cal = sum;
-			cnt = cnt + 1;
-		}
+
+		Scanner sc = new Scanner(System.in);
+		num = sc.next();
 		
-		System.setOut(new PrintStream(new BufferedOutputStream(System.out)));
+		String a = num.substring(1);
+		parseInt
+
 		System.out.println(cnt);
 	}
 }
